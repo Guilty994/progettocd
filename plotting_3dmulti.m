7a0bb2d40bb0e8e16f709205fce2fa2f6e8f2cbc
lucamadd <pro.luca.years@gmail.com>
2021-12-31 13:40:27 +0100

    Fixed codec position

--- plotting_3dmulti.m
+++ plotting_3dmulti.m
@@ -5,14 +5,14 @@ jpeg_2000_real = mean(jpeg2000_multi3d_SNR_real)
 libaom_av1_real = mean(libaom_av1_multi3d_SNR_real)
 svt_av1_real = mean(svt_av1_multi3d_SNR_real)
 
-means_real = [hevc_real,jpeg_2000_real,libaom_av1_real,svt_av1_real]
+means_real = [hevc_real,jpeg_2000_real,svt_av1_real,libaom_av1_real]
 
 hevc_imag = mean(hevc_multi3d_SNR_imag)
 jpeg_2000_imag = mean(jpeg2000_multi3d_SNR_imag)
 libaom_av1_imag = mean(libaom_av1_multi3d_SNR_imag)
 svt_av1_imag = mean(svt_av1_multi3d_SNR_imag)
 
-means_imag = [hevc_imag,jpeg_2000_imag,libaom_av1_imag,svt_av1_imag]
+means_imag = [hevc_imag,jpeg_2000_imag,svt_av1_imag,libaom_av1_imag]
 
 
 data_real = means_real';
@@ -22,7 +22,7 @@ data_imag = means_imag';
     ylabel('SNR [dB]')
     grid on;
     grid minor;
-    set(gca,'XTickLabel',{'HEVC','JPEG 2000','libaom AV1','SVT-AV1'});
+    set(gca,'XTickLabel',{'HEVC','JPEG 2000','SVT-AV1','libaom AV1'});
     legend('real', 'imag');
 
 
